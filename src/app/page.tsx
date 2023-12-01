a172fdfb76762636edd7a0b7d4130e0b234b4f55
Abraham-Ogbumah <ab.ogbumah@gmail.com>
2023-12-01 08:27:46 -0500

    update to home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,16 @@
-import { Button } from '@/components/ui/button'
-import Image from 'next/image'
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
 
 export default function Home() {
   return (
-    <Button>Click Me</Button>
-  )
+    <div className="bg-gradient-to-r min-h-screen grainy from-rose-100 to-teal-100">
+      <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+        <h1 className="font-semibold text-7x1 text-center">
+          AI <span className="text-green-600 font-bold">note taking assistant</span>
+        </h1>
+        <div className="mt-4"></div>
+        <h2 className="font-semibold text-3xl text-center text-slate-700">AI Powered</h2>
+      </div>
+    </div>
+  );
 }
